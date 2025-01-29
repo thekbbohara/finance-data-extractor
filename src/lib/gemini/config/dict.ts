588306612e9a2729a6f0e2e35a5c50ab2edf76e7
thekbbohara <ytkbzcraft@gmail.com>
2025-01-30 02:38:58 +0545

    orgin name

---
+++ src/lib/gemini/config/dict.ts
@@ -0,0 +1,148 @@
+// Original name
+export const cashFlowDict: { [key: string]: string } = {
+  interest_received: 'Interest received',
+  fees_and_other_income_received: 'Fees and other income received',
+  dividend_received: 'Dividend received',
+  receipts_from_other_operating_activities:
+    'Receipts from other operating activities',
+  interest_paid: 'Interest paid',
+  commission_and_fees_paid: 'Commission and fees paid',
+  cash_payment_to_employees: 'Cash payment to employees',
+  other_expense_paid: 'Other expense paid',
+  operating_cash_flows_before_changes:
+    'Operating cash flows before changes in operating assets and liabilities',
+  increase_in_operating_assets_due_from_nepal_rastra_bank:
+    'Increase in operating assets: Due from Nepal Rastra Bank',
+  increase_in_operating_assets_placement_with_banks:
+    'Increase in operating assets: Placement with bank and financial institutions',
+  increase_in_operating_assets_other_trading_assets:
+    'Increase in operating assets: Other trading assets',
+  increase_in_operating_assets_loans_to_banks:
+    'Increase in operating assets: Loan and advances to bank and financial institutions',
+  increase_in_operating_assets_loans_to_customers:
+    'Increase in operating assets: Loans and advances to customers',
+  increase_in_operating_assets_other_assets:
+    'Increase in operating assets: Other assets',
+  increase_in_operating_liabilities_due_to_banks:
+    'Increase in operating liabilities: Due to bank and financial institutions',
+  increase_in_operating_liabilities_due_to_nepal_rastra_bank:
+    'Increase in operating liabilities: Due to Nepal Rastra Bank',
+  increase_in_operating_liabilities_deposits_from_customers:
+    'Increase in operating liabilities: Deposit from customers',
+  increase_in_operating_liabilities_borrowings:
+    'Increase in operating liabilities: Borrowings',
+  increase_in_operating_liabilities_other_liabilities:
+    'Increase in operating liabilities: Other liabilities',
+  net_cash_flow_from_operating_activities_before_tax:
+    'Net cash flow from operating activities before tax paid',
+  income_taxes_paid: 'Income taxes paid',
+  net_cash_flow_from_operating_activities:
+    'Net cash flow from operating activities',
+  purchase_of_investment_securities: 'Purchase of investment securities',
+  receipts_from_sale_of_investment_securities:
+    'Receipts from sale of investment securities',
+  purchase_of_property_and_equipment: 'Purchase of property and equipment',
+  receipt_from_sale_of_property_and_equipment:
+    'Receipt from the sale of property and equipment',
+  purchase_of_intangible_assets: 'Purchase of intangible assets',
+  receipt_from_sale_of_intangible_assets:
+    'Receipt from the sale of intangible assets',
+  purchase_of_investment_properties: 'Purchase of investment properties',
+  receipt_from_sale_of_investment_properties:
+    'Receipt from the sale of investment properties',
+  interest_received_investing: 'Interest received (investing activities)',
+  dividend_received_investing: 'Dividend received (investing activities)',
+  net_cash_used_in_investing_activities:
+    'Net cash used in investing activities',
+  receipt_from_issue_of_debt_securities:
+    'Receipt from issue of debt securities',
+  repayment_of_debt_securities: 'Repayment of debt securities',
+  receipt_from_issue_of_subordinated_liabilities:
+    'Receipt from issue of subordinated liabilities',
+  repayment_of_subordinated_liabilities:
+    'Repayment of subordinated liabilities',
+  receipt_from_issue_of_shares: 'Receipt from issue of shares',
+  dividends_paid: 'Dividends paid',
+  interest_paid_financing: 'Interest paid (financing activities)',
+  other_receipt_payment_financing:
+    'Other receipt/payment (financing activities)',
+  net_cash_from_financing_activities: 'Net cash from financing activities',
+  net_increase_in_cash_and_cash_equivalents:
+    'Net increase (decrease) in cash and cash equivalents',
+  cash_and_cash_equivalents_at_beginning:
+    'Cash and cash equivalents at Sawan 1, 2081',
+  effect_of_exchange_rate_fluctuations:
+    'Effect of exchange rate fluctuations on cash and cash equivalents held',
+  cash_and_cash_equivalents_at_end:
+    'Cash and cash equivalents at 30 Ashwin 2081'
+};
+
+export const profitLossDict: { [key: string]: string } = {
+  interest_income: 'Interest Income',
+  interest_expense: 'Interest Expense',
+  net_interest_income: 'Net Interest Income',
+  fee_and_commission_income: 'Fee and Commission Income',
+  fee_and_commission_expense: 'Fee and Commission Expense',
+  net_fee_and_commission_income: 'Net Fee and Commission Income',
+  net_interest_fee_and_commission_income:
+    'Net Interest, Fee, and Commission Income',
+  net_trading_income: 'Net Trading Income',
+  other_operating_income: 'Other Operating Income',
+  total_operating_income: 'Total Operating Income',
+  impairment_charge_reversal_for_loans_and_other_losses:
+    'Impairment Charge/Reversal for Loans and Other Losses',
+  net_operating_income: 'Net Operating Income',
+  personnel_expense: 'Personnel Expenses',
+  other_operating_expenses: 'Other Operating Expenses',
+  depreciation_amortization: 'Depreciation and Amortization',
+  operating_profit: 'Operating Profit',
+  non_operating_income: 'Non-Operating Income',
+  non_operating_expense: 'Non-Operating Expense',
+  profit_before_income_tax: 'Profit Before Income Tax',
+  current_tax: 'Current Tax',
+  deferred_tax: 'Deferred Tax',
+  profit_loss_for_the_period: 'Net Profit'
+};
+
+export const balanceSheetDict: { [key: string]: string } = {
+  cash_and_cash_equivalents: 'Cash and Cash Equivalents',
+  due_from_nepal_rastra_bank: 'Due from Nepal Rastra Bank',
+  placement_with_bank_and_financial_institutions:
+    'Placement with Bank and Financial Institutions',
+  assets_derivative_financial_instruments:
+    'Assets: Derivative Financial Instruments',
+  other_trading_assets: 'Other Trading Assets',
+  loan_and_advances_to_bfs:
+    'Loan and Advances to Bank and Financial Institutions',
+  loan_and_advances_to_customers: 'Loans and Advances to Customers',
+  investment_securities: 'Investment Securities',
+  current_tax_asset: 'Current Tax Asset',
+  investment_in_subsidiaries: 'Investment in Subsidiaries',
+  investment_in_associates: 'Investment in Associates',
+  investment_property: 'Investment Property',
+  property_and_equipment: 'Property and Equipment',
+  goodwill_and_intangible_assets: 'Goodwill and Intangible Assets',
+  deferred_tax_assets: 'Deferred Tax Assets',
+  other_assets: 'Other Assets',
+  total_assets: 'Total Assets',
+  due_to_bank_and_financial_institutions:
+    'Due to Bank and Financial Institutions',
+  due_to_nepal_rastra_bank: 'Due to Nepal Rastra Bank',
+  liabilities_derivative_financial_instruments:
+    'Liabilities: Derivative Financial Instruments',
+  deposit_from_customers: 'Deposits from Customers',
+  borrowings: 'Borrowings',
+  current_tax_liabilities: 'Current Tax Liabilities',
+  provisions: 'Provisions',
+  deferred_tax_liabilities: 'Deferred Tax Liabilities',
+  other_liabilities: 'Other Liabilities',
+  debt_securities_issued: 'Debt Securities Issued',
+  subordinated_liabilities: 'Subordinated Liabilities',
+  total_liabilities: 'Total Liabilities',
+  share_capital: 'Share Capital',
+  share_premium: 'Share Premium',
+  retained_earnings: 'Retained Earnings',
+  reserves: 'Reserves',
+  total_equity: 'Total Equity',
+  total_liabilities_and_equity: 'Total Liabilities and Equity'
+};
